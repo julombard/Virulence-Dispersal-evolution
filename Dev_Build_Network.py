df43e7ec049720bc5a6d21471fc86553bcadf88e
jlombard <j.lombard703@laposte.net>
2022-09-06 15:48:23 +0200

    UPDATE : Added some networks configurations : - Square Lattice - Hexagonal Lattice - Accordéon k-neighbors (why not) - Random Network with k neighbors (with euclidian distances AND/OR Connectivity matrix)

--- Dev_Build_Network.py
+++ Dev_Build_Network.py
@@ -12,6 +12,8 @@ Sites = [0,1,2,3,4,5,6,7,8,9]
 NbSites = len(Sites)
 Nb_Neighbors = 4
 
+### CONSTRUCTION D'UN RESEAU A PARTIR DE SITES ADJACENTS DANS UNE LISTE
+
 def Build_Accordion_Neighboring(Listsites, Nb_neighbors ) : # ListSites is a list of sites objects, Nb_neighbors is an Int
     List_indexes = []
     for i in Listsites :
@@ -34,12 +36,8 @@ def Build_Accordion_Neighboring(Listsites, Nb_neighbors ) : # ListSites is a lis
                         dico_adjacency[f"{i}"].append(Listsites[Index_current + j])
     return dico_adjacency
 
-#Mon_raizo = Build_Neighboring(Sites, Nb_Neighbors)
-#print(Mon_raizo)
 
-
-
-### FONCTION QUI FAIT UN RESEAU ALEATOIRE AVEC POSSIBLES DISTANCES GEOGRAPHIQUES
+### CONSTRUCTION D'UN RESEAU ALEATOIRE AVEC DISTANCE EUCLIDIENNES OU MATRICE DE CONNECTIVITE
 Sites = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 def Set_kneigh_random_network(List_sites) :
 
@@ -136,10 +134,10 @@ dico_coordSites, liste_des_sites,  Distances_entre_sites, Normalised_distance_ma
 #print("2",liste_des_sites)
 #print("3",Distances_entre_sites)
 #print("4", Normalised_distance_matrix)
-#print(Normalised_distance_matrix.sum())
+#print(Normalised_distance_matrix.sum()) Attention ça ne fait 1 que sur la somme des lignes (Si A est PPV de B, la réciproque marche pas toujours)
 #print(Conn_Mat)
-import networkx as nx
-nx.grid_2d_graph
+# TOUT FONCTIONNE MAWAHAHAHAHAHAH
+
 
 ###### Algorithm for hexagonal lattice grid (each site IS an hexagon, six neighbors) #####
 nb_sites = 25
@@ -154,7 +152,9 @@ for i in range(nb_sites):
 # 4. Deal with the boundary conditions if you want to.
 # 5. That's all
 
-def Build_Hexagonal_Regular_Grid(n,p): # Takes as parameters a number of sites per row (n) et per column (p)
+# CONSTRUCTION D'UN RESEAU REGULIER A 4 VOISINS, LATTICE CARRE
+
+def Build_Square_Lattice(n,p): # Takes as parameters a number of sites per row (n) et per column (p)
     nb_sites = n*p
     dico_coordSites = {}
     #First we explicit each site and assign them a vector of coordinates (x,y) that is their position on the grid
@@ -163,9 +163,27 @@ def Build_Hexagonal_Regular_Grid(n,p): # Takes as parameters a number of sites p
         for j in range(p) : # And each column
             dico_coordSites[Site_counter]= [i,j]
             Site_counter += 1
-    #Now that the nodes are in place, we add egdes as if we wanted to fill a square lattice (with borders, for now)
+    #Now that the nodes are in place, we add edges as if we wanted to fill a square lattice (with borders, for now)
+    row_edges = Pairwise_association(dico_coordSites, "Row", n)
+    col_edges = Pairwise_association(dico_coordSites, "Column", p)
 
-    return dico_coordSites
+    #From this we build an adjacency dictionnary, which at each site makes correspond a list of neighbors
+    dico_neighborhood = {}
+    for i in range(nb_sites) :
+        dico_neighborhood[f"{i}"]= []
+        for pair in row_edges :
+            if i in pair :
+                paircopy = deepcopy(pair)
+                paircopy.remove(i)
+                dico_neighborhood[f"{i}"].append(paircopy[0])
+        for pair in col_edges :
+            if i in pair :
+                paircopy = deepcopy(pair)
+                paircopy.remove(i)
+                dico_neighborhood[f"{i}"].append(paircopy[0])
+
+
+    return dico_neighborhood
 
 def Pairwise_association(Coordinates,Type, length) : # Coordinate is a Dict object with Site = (x,y), Type is an str() that takes "Rows" or "Columns" values, length is the (int) length associated
     pairs = []
@@ -191,10 +209,69 @@ def Pairwise_association(Coordinates,Type, length) : # Coordinate is a Dict obje
     return pairs
 
 
-Dico = Build_Hexagonal_Regular_Grid(5,5)
+Dico = Build_Square_Lattice(5,5)
 print(Dico)
 print("j'ai perdu mes cles", Dico.keys())
-print(len(Dico))
-Row_pairs = Pairwise_association(Dico, "Column", 5)
-print(Row_pairs)
 
+
+# CONSTRUCTION D'UN RESEAU REGULIER A 6 VOISINS, HEXAGONAL LATTICE
+
+def Build_Hexagonal_Lattice(n,p): # Takes as parameters a number of sites per row (n) et per column (p)
+    nb_sites = n*p
+    dico_coordSites = {}
+    #First we explicit each site and assign them a vector of coordinates (x,y) that is their position on the grid
+    Site_counter = 0
+    for i in range(n) : # For each row
+        for j in range(p) : # And each column
+            dico_coordSites[Site_counter]= [i,j]
+            Site_counter += 1
+    #Now that the nodes are in place, we add edges as if we wanted to fill a square lattice (with borders, for now)
+    row_edges = Pairwise_association(dico_coordSites, "Row", n)
+    col_edges = Pairwise_association(dico_coordSites, "Column", p)
+
+    #Complete the diagonal edges in order to get the hexagonal lattice
+    Diagonal_edges = []
+    List_keys = list(dico_coordSites.keys())
+    List_values = list(dico_coordSites.values())
+    for i in range(len(dico_coordSites)): # For each node
+        coord = dico_coordSites[i] # We get its coordinate
+        print("HERE COME THE X", coord[0])
+        if coord[0] % 2 == 0 : # If the x component is even
+            diagonal_left_neighbor = [coord[0]+1, coord[1]-1] # we get the site in (x+1, y-1)
+            if diagonal_left_neighbor in dico_coordSites.values() : # if it exists
+                Index_neighbor = List_values.index(diagonal_left_neighbor)
+                Diagonal_edges.append([i, List_keys[Index_neighbor]])
+
+        if coord[0] % 2 != 0 : # If the x component is odd
+            diagonal_right_neighbor = [coord[0] + 1, coord[1] + 1]  # we get the site in (x+1, y-1)
+            if diagonal_right_neighbor in dico_coordSites.values():  # if it exists
+                Index_neighbor = List_values.index(diagonal_right_neighbor)
+                Diagonal_edges.append([i, List_keys[Index_neighbor]])
+
+
+
+    #From this we build an adjacency dictionnary, which at each site makes correspond a list of neighbors
+    dico_neighborhood = {}
+    for i in range(nb_sites) :
+        dico_neighborhood[f"{i}"]= []
+        for pair in row_edges :
+            if i in pair :
+                paircopy = deepcopy(pair)
+                paircopy.remove(i)
+                dico_neighborhood[f"{i}"].append(paircopy[0])
+        for pair in col_edges :
+            if i in pair :
+                paircopy = deepcopy(pair)
+                paircopy.remove(i)
+                dico_neighborhood[f"{i}"].append(paircopy[0])
+        for pair in Diagonal_edges :
+            if i in pair :
+                paircopy = deepcopy(pair)
+                paircopy.remove(i)
+                dico_neighborhood[f"{i}"].append(paircopy[0])
+
+
+    return dico_neighborhood
+
+DicoH = Build_Hexagonal_Lattice(5,5)
+print(DicoH)
